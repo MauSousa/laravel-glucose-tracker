0132d48f4d0a7adae9caa67502e40fb8fa1ee2c6
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-11-09 19:56:04 -0600

    chore: add condition enum

---
+++ app/Enums/Condition.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+enum Condition: string
+{
+    case Ayuno = 'Ayuno';
+    case Desayuno = 'Desayuno';
+    case Comida = 'Comida';
+    case Cena = 'Cena';
+}
