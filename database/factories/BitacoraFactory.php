b442cb672b929b1621abc5c28bd661cba54ad37d
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-11-09 20:15:11 -0600

    chore: add values to factory

--- database/factories/BitacoraFactory.php
+++ database/factories/BitacoraFactory.php
@@ -1,7 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Factories;
 
+use App\Enums\Condition;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +21,11 @@ class BitacoraFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'day' => \Illuminate\Support\Facades\Date::now()->dayName,
+            'time_of_test' => \Illuminate\Support\Facades\Date::now()->format('H:i:s'),
+            'glucose' => $this->faker->randomDigitNotZero(),
+            'condition' => $this->faker->randomElement(Condition::cases()),
         ];
     }
 }
