697a7f0e6f183e65817b18ac99b82cd6da606e99
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-11-09 20:15:00 -0600

    chore: add database structure

--- database/migrations/2025_11_08_200358_create_bitacoras_table.php
+++ database/migrations/2025_11_08_200358_create_bitacoras_table.php
@@ -1,5 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Enums\Condition;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,8 +14,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('bitacoras', function (Blueprint $table) {
+        Schema::create('bitacoras', function (Blueprint $table): void {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('day');
+            $table->time('time_of_test');
+            $table->unsignedInteger('glucose');
+            $table->enum('condition', Condition::cases());
             $table->timestamps();
         });
     }
