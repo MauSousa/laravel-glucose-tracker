a6bbb09db64aed2c188f81918f55c205383b9cbf
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-11-09 20:16:19 -0600

    chore: add views to controller logic

--- app/Http/Controllers/BitacoraController.php
+++ app/Http/Controllers/BitacoraController.php
@@ -1,33 +1,36 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreBitacoraRequest;
 use App\Http\Requests\UpdateBitacoraRequest;
 use App\Models\Bitacora;
+use Illuminate\View\View;
 
 class BitacoraController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
-        //
+        return view('bitacora.index');
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('bitacora.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreBitacoraRequest $request)
+    public function store(StoreBitacoraRequest $request): void
     {
         //
     }
@@ -35,7 +38,7 @@ class BitacoraController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Bitacora $bitacora)
+    public function show(Bitacora $bitacora): void
     {
         //
     }
@@ -43,15 +46,15 @@ class BitacoraController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Bitacora $bitacora)
+    public function edit(Bitacora $bitacora): View
     {
-        //
+        return view('bitacora.edit', $bitacora);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateBitacoraRequest $request, Bitacora $bitacora)
+    public function update(UpdateBitacoraRequest $request, Bitacora $bitacora): void
     {
         //
     }
@@ -59,7 +62,7 @@ class BitacoraController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Bitacora $bitacora)
+    public function destroy(Bitacora $bitacora): void
     {
         //
     }
