b174ebbe9be92cd0b6617eb72d4a4f23d495cad4
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-11-10 14:39:22 -0600

    feat: add validation for storing data

--- app/Http/Requests/StoreBitacoraRequest.php
+++ app/Http/Requests/StoreBitacoraRequest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
+use App\Enums\Condition;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreBitacoraRequest extends FormRequest
 {
@@ -13,7 +15,7 @@ class StoreBitacoraRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +26,11 @@ class StoreBitacoraRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'day' => ['required', Rule::date()->format('d-m-Y')],
+            'time_of_test' => ['required', Rule::date()->format('H:i:s')],
+            'glucose' => ['required', 'integer:strict', 'min:0'],
+            'condition' => ['required', Rule::enum(Condition::class)],
+            'food' => ['required', 'string', 'min:0', 'max:255'],
         ];
     }
 }
