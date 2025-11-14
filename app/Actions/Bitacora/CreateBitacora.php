b5713012a3930a8f174fd478f79d67765057adf8
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-11-14 16:03:13 -0600

    fix: change return type

--- app/Actions/Bitacora/CreateBitacora.php
+++ app/Actions/Bitacora/CreateBitacora.php
@@ -6,6 +6,7 @@ namespace App\Actions\Bitacora;
 
 use App\Models\Bitacora;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateBitacora
 {
@@ -14,7 +15,7 @@ class CreateBitacora
      *
      * @param  array<mixed,string>  $data
      */
-    public function handle(User $user, array $data): Bitacora
+    public function handle(User $user, array $data): Model
     {
         return $user->bitacora()->create($data);
     }
