e9255c5a82d0d4351b61bf7aca978b613cff48e4
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-11-14 16:10:16 -0600

    feat: add eloquent relationship and propertie casting

--- app/Models/Bitacora.php
+++ app/Models/Bitacora.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Bitacora extends Model
 {
@@ -13,4 +14,20 @@ class Bitacora extends Model
     use HasFactory;
 
     protected $fillable = ['user_id', 'day', 'time_of_test', 'glucose', 'condition', 'food'];
+
+    protected $casts = [
+        'day' => 'date:d-m-Y',
+        'created_at' => 'datetime:d-m-Y',
+        'updated_at' => 'datetime:d-m-Y',
+    ];
+
+    /**
+     * Get the user that belongs the bitacora
+     *
+     * @return BelongsTo<User,$this>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
